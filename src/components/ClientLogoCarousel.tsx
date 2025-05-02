3ee9d6dda888f62ebb2393c3f1d4813940d0ff31
melt-sanskar <sanskar.meltdown@gmail.com>
2025-05-02 15:05:06 +0545

    video

--- src/components/ClientLogoCarousel.tsx
+++ src/components/ClientLogoCarousel.tsx
@@ -1,3 +1,75 @@
+<<<<<<< HEAD
+import React, { useEffect, useRef } from 'react';
+const clientLogos = [{
+  id: 1,
+  src: "/lovable-uploads/leapfrog.png",
+  alt: "Client Logo 1"
+}, {
+  id: 2,
+  src: "/lovable-uploads/SecurityPal.png",
+  alt: "Client Logo 2"
+}, {
+  id: 3,
+  src: "/lovable-uploads/naamche.png",
+  alt: "Client Logo 3"
+}, {
+  id: 4,
+  src: "/lovable-uploads/flextacs.png",
+  alt: "Client Logo 4"
+}, {
+  id: 5,
+  src: "/lovable-uploads/YoungInnovation.png",
+  alt: "Client Logo 5"
+}, {
+  id: 6,
+  src: "/lovable-uploads/HimalayanJava.png",
+  alt: "Client Logo 6"
+}, {
+  id: 7,
+  src: "/lovable-uploads/GolchhaGroup.png",
+  alt: "Client Logo 7"
+}, {
+  id: 8,
+  src: "/lovable-uploads/ottr.png",
+  alt: "Client Logo 8"
+}, {
+  id: 9,
+  src: "/lovable-uploads/OwlDigital.png",
+  alt: "Client Logo 9"
+}, {
+  id: 10,
+  src: "/lovable-uploads/shanker.png",
+  alt: "Client Logo 10"
+},{
+  id: 11,
+  src: "/lovable-uploads/KingsCollege.png",
+  alt: "Client Logo 10"
+},{
+  id: 12,
+  src: "/lovable-uploads/veda.png",
+  alt: "Client Logo 10"
+},{
+  id: 13,
+  src: "/lovable-uploads/CodingMountain.png",
+  alt: "Client Logo 10"
+},{
+  id: 14,
+  src: "/lovable-uploads/JagadambaMotors.png",
+  alt: "Client Logo 10"
+},{
+  id: 15,
+  src: "/lovable-uploads/OwlDigital.png",
+  alt: "Client Logo 10"
+},{
+  id: 16,
+  src: "/lovable-uploads/KavyaSports.png",
+  alt: "Client Logo 10"
+},{
+  id: 17,
+  src: "/lovable-uploads/Stalwart.png",
+  alt: "Client Logo 10"
+}];
+=======
 
 import React, { useEffect, useRef, useState } from 'react';
 
@@ -89,6 +161,7 @@ const clientLogos = [
   }
 ];
 
+>>>>>>> 27c7597fc1d3fe047ed4aed14fc4130a665e1b51
 const ClientLogoCarousel = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
   const [isVisible, setIsVisible] = useState(false);
